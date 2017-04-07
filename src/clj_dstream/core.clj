cdabdc5450fc0803464a4372cbb11741ab26de54
Octavian Geagla <ogeagla@gmail.com>
2017-04-07 14:15:24 -0600

    [spec] use characteristic vector instead of time series

--- src/clj_dstream/core.clj
+++ src/clj_dstream/core.clj
@@ -50,9 +50,16 @@
 
 (s/def ::time-points (s/coll-of ::time-point))
 
-(s/def ::grid-cell (s/keys :req [::position-value ::position-index ::time-points]))
+(s/def ::last-time-update-time int?)
+(s/def ::last-time-removed-as-sporadic int?)
+(s/def ::grid-density-at-last-update float?)
+(s/def ::sporadic-or-normal #{::sporadic ::normal})
 
-(s/def ::grid-cells (s/coll-of ::grid-cell))
+(s/def ::char-vec (s/keys :req [::last-update-time ::last-time-removed-as-sporadic ::grid-density-at-last-update ::sporadic-or-normal ::cluster-label]))
+
+(s/def ::grid-cell (s/keys :req [::position-value ::position-index ::char-vec]))
+
+(s/def ::grid-cells (s/map-of ::position-index ::char-vec))
 
 (s/def ::gap_time pos-int?)
 
@@ -77,16 +84,14 @@
                              ::initialized-clusters]))
 
 
-(s/def ::raw-data (s/keys :req [::value ::position-value]))
+(s/def ::raw-datum (s/keys :req [::value ::position-value]))
 
 (def test-state
-  {::grid-cells           [{::position-value [0.1 1.0 2.0 3.0]
-                            ::position-index [0 1 2 3]
-                            ::time-points    [{::t             0
-                                            ::value         0.1
-                                            ::status        ::sporadic
-                                            ::label         ::dense
-                                            ::cluster-label "C0"}]}]
+  {::grid-cells           {[0 1 2 3] {::last-update-time              0
+                                      ::last-time-removed-as-sporadic 0
+                                      ::grid-density-at-last-update   0.2
+                                      ::sporadic-or-normal            ::normal
+                                      ::cluster-label                 nil}}
    ::properties           {::c_m        0.1
                            ::c_l        0.2
                            ::lambda     0.99
@@ -94,16 +99,16 @@
                            ::dimensions 4
                            ::domains    [
                                          {::domain-start    -1.0
-                                          ::domain-end      1.0
+                                          ::domain-end      10.0
                                           ::domain-interval 0.1}
                                          {::domain-start    -1.0
-                                          ::domain-end      1.0
+                                          ::domain-end      10.0
                                           ::domain-interval 0.1}
                                          {::domain-start    -1.0
-                                          ::domain-end      1.0
+                                          ::domain-end      10.0
                                           ::domain-interval 0.1}
                                          {::domain-start    -1.0
-                                          ::domain-end      1.0
+                                          ::domain-end      10.0
                                           ::domain-interval 0.1}]
                            ::gap_time   4}
    ::initialized-clusters true})
@@ -112,13 +117,89 @@
   {::value          0.1
    ::position-value [5.0 5.0 5.0 5.0]})
 
-(defn one-dstream-iteration [{:keys [::state ::raw-data]}]
-  state)
+(defn position-value->position-index [{:keys [::position-value ::domains]}]
+  (if-not (= (count position-value) (count domains))
+    (throw (ex-info "Dimension mismatch" {:keys :data position-value domains})))
+  (->>
+    position-value
+    (map-indexed (fn [idx position-val]
+                   (let [{:keys [::domain-start ::domain-end ::domain-interval]} (get domains idx)]
+                     (if (or (> position-val domain-end)
+                             (< position-val domain-start))
+                       (throw
+                         (ex-info
+                           "Raw data value outside range" {:keys idx
+                                                           :data {:value  position-val
+                                                                  :domain {::domain-interval domain-interval
+                                                                           ::domain-start    domain-start
+                                                                           ::domain-end      domain-end}}})))
+                     (let [dist        (- position-val domain-start)
+                           value-index (int (/ dist domain-interval))]
+                       value-index))))
+    vec))
+
+(defn put [{:keys [::raw-datum ::state ::t]}]
+  (let [idx (position-value->position-index (merge raw-datum (::properties state)))]
+    (if (contains? (::grid-cells state) idx)
+      (let [char-vec  (get-in state [::grid-cells idx])
+            new-d     (+ 1.0
+                         (*
+                           (::grid-density-at-last-update char-vec)
+                           (Math/pow (get-in state [::properties ::lambda])
+                                     (- t
+                                        (::last-update-time char-vec)))))
+            new-cv    (assoc char-vec
+                        ::last-update-time t
+                        ::grid-density-at-last-update new-d)
+            new-state (assoc-in state [::grid-cells idx] new-cv)]
+        (println "updated grid new state: " idx)
+        {::state new-state})
+      (let [new-state (assoc-in state
+                                [::grid-cells idx]
+                                {::last-update-time              t
+                                 ::last-time-removed-as-sporadic 0
+                                 ::grid-density-at-last-update   1.0
+                                 ::sporadic-or-normal            ::normal
+                                 ::cluster-label                 nil})]
+        (println "new grid new state: " idx)
+        {::state new-state}))))
+
+(defn one-dstream-iteration [{:keys [::state ::raw-datum ::t] :as data}]
+  (let [state-after-put (put data)]
+    state-after-put))
+
+(defn dstream-iterations [state raw-data]
+  (println "iters w state:")
+  (clojure.pprint/pprint state)
+  (println "raw data: ")
+  (clojure.pprint/pprint raw-data)
+  (let [time*      (atom 0)
+        the-state* (atom state)]
+    (doseq [raw-datum raw-data]
+
+      (reset! the-state* (one-dstream-iteration (merge @the-state* raw-datum {::t @time*})))
+      (swap! time* inc))
+    @the-state*))
 
 (s/fdef one-dstream-iteration
-        :args (s/cat :u (s/keys :req [::state ::raw-data]))
+        :args (s/cat :u (s/keys :req [::state ::raw-datum]))
         :ret (s/keys :req [::state]))
 
-(stest/instrument `one-dstream-iteration)
+(s/fdef put
+        :args (s/cat :u (s/keys :req [::state ::raw-datum ::t]))
+        :ret (s/keys :req [::state]))
 
-(clojure.pprint/pprint (one-dstream-iteration {::state test-state ::raw-data test-raw-data}))
+(s/fdef position-value->position-index
+        :args (s/cat :u (s/keys :req [::position-value ::domains]))
+        :ret ::position-index)
+
+;(s/fdef dstream-iterations
+;        :args (s/cat :u (s/keys :req [::state ::raw-datum]))
+;        :ret ::state)
+
+(stest/instrument `one-dstream-iteration)
+(stest/instrument `position-value->position-index)
+(stest/instrument `put)
+;(stest/instrument `dstream-iterations)
+
+;(clojure.pprint/pprint (one-dstream-iteration {::state test-state ::raw-data test-raw-data}))
--- test/clj_dstream/core_test.clj
+++ test/clj_dstream/core_test.clj
@@ -1,7 +1,25 @@
 (ns clj-dstream.core-test
   (:require [clojure.test :refer :all]
-            [clj-dstream.core :refer :all]))
+            [clj-dstream.core :as core]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+
+
+(deftest put-test
+  (testing "Puts raw data into state"
+    (let [test-state    {::core/state core/test-state}
+          test-raw-data {::core/raw-datum core/test-raw-data}
+
+          put-state     (core/put (merge test-state test-raw-data {::core/t      2
+                                                                   ::core/lambda 0.3}))
+          put-state-2   (core/put (merge put-state test-raw-data {::core/t      3
+                                                                  ::core/lambda 0.3}))
+          ]
+      (println "final state: " put-state-2))
+    ))
+
+(deftest dstream-iterations
+  (testing "Dstream iterations"
+    (let [test-state    {::core/state core/test-state}
+          test-raw-data (repeat 10 {::core/raw-datum core/test-raw-data})
+          final-state   (core/dstream-iterations test-state test-raw-data)]
+      (clojure.pprint/pprint final-state))))
